9a217fb31a3cfcc693a0d9f24ee1b2e8afb2e2ea
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2022-05-03 18:07:04 -0500

    random function added

--- template/random.py
+++ template/random.py
@@ -5,23 +5,22 @@ gpa(0-4.0), internship(0-100), research(0-100)
 '''
 import random
 
-def decision(choose, gpa, internship, research):
+
+def decision(isJob, gpa, internship, research):
     num = 0
     result = False
-    if choose == "job":
+    if isJob:
         # job: gpa -> 30%
         num = (gpa / 4.0) * 0.3 + (internship / 100)
         result = random_calculator(num)
-    elif choose == "grad":
+    else:
         # grad school: gpa -> 60%
         num = (gpa / 4.0) * 0.6 + (research / 100)
         result = random_calculator(num)
-    else:
-        return "This choice is not available in this game"
-    
-    if result and choose == "job":
+
+    if result and isJob == "job":
         return "Congrats! You get your deam job!"
-    elif result and choose == "grad":
+    elif result and not isJob == "grad":
         return "Congrats! You made your deam grad school!"
     return "There were several applications submitted for this position, and after careful review, unfortunately, we have decided to pursue a different candidate whose experience and skills more closely meet the needs of this particular role."
 
